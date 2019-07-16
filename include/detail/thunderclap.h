d5cb918709b4fba3ed0c6bf0b94f76309a1b1f3f
Akira <akira.matsuda@me.com>
2019-07-17 02:48:10 +0900

    format

--- include/detail/thunderclap.h
+++ include/detail/thunderclap.h
@@ -10,70 +10,82 @@
 #include <algorithm>
 #include <random>
 
-namespace thunderclap
+namespace m2d
 {
-	template <typename T> thunderclap<T>* thunderclap<T>::shared_instance() {
-		static thunderclap::thunderclap<T> shared_instance;
-		return &shared_instance;
+template <typename T>
+thunderclap<T> *thunderclap<T>::shared_instance()
+{
+	static thunderclap<T> shared_instance;
+	return &shared_instance;
+}
+
+template <typename T>
+bool thunderclap<T>::available(std::string name)
+{
+	bool result = false;
+	if (this->table.find(name) != this->table.end()) {
+		result = true;
 	}
 
-	template <typename T> bool thunderclap<T>::available(std::string name) {
-		bool result = false;
-		if (this->table.find(name) != this->table.end()) {
-			result = true;
-		}
+	return result;
+}
 
-		return result;
-	}
-
-	template <typename T> void thunderclap<T>::clap(std::string name, T x) {
-		if (this->available(name)) {
-			auto m = this->table[name];
-			for (auto it = m->begin(); it != m->end(); ++it) {
-				auto f = it->second;
-				f(x);
-			}
+template <typename T>
+void thunderclap<T>::clap(std::string name, T x)
+{
+	if (this->available(name)) {
+		auto m = this->table[name];
+		for (auto it = m->begin(); it != m->end(); ++it) {
+			auto f = it->second;
+			f(x);
 		}
 	}
+}
 
-	template <typename T> std::string thunderclap<T>::watch(std::string name, std::function<void(T x)> f) {
-		std::string hash;
-		static std::string chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789,./;'[]-=<>?:{}|_+";
-		static std::random_device rnd;
-		static std::mt19937 mt(rnd());
-		static std::uniform_int_distribution<> idx(0, 32);
-		for (int i = 0; i < 32; ++i) {
-			hash += chars[idx(mt)];
-		}
-		
-		if (this->available(name)) {
-			auto m = this->table[name];	
-			m->insert(std::make_pair(hash, f));
-			this->table[name] = m;
-		}
-		else {
-			std::map<std::string, std::function<void(T x)>> m;
-			m.insert(std::make_pair(hash, f));
-			this->table[name] = std::make_shared<std::map<std::string, std::function<void(T x)>>>(m);
-		}
-
-		return hash;
+template <typename T>
+std::string thunderclap<T>::watch(std::string name, std::function<void(T x)> f)
+{
+	std::string hash;
+	static std::string chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789,./;'[]-=<>?:{}|_+";
+	static std::random_device rnd;
+	static std::mt19937 mt(rnd());
+	static std::uniform_int_distribution<> idx(0, 32);
+	for (int i = 0; i < 32; ++i) {
+		hash += chars[idx(mt)];
 	}
 
-	template <typename T> void thunderclap<T>::dispose(std::string name, std::string hash) {
-		if (this->available(name)) {
-			auto m = this->table[name];
-			auto it = m->find(hash);
-			if (it != m->end()) {
-				m->erase(it);
-			}
-		}
+	if (this->available(name)) {
+		auto m = this->table[name];
+		m->insert(std::make_pair(hash, f));
+		this->table[name] = m;
+	}
+	else {
+		std::map<std::string, std::function<void(T x)>> m;
+		m.insert(std::make_pair(hash, f));
+		this->table[name] = std::make_shared<std::map<std::string, std::function<void(T x)>>>(m);
 	}
 
-	template <typename T> void thunderclap<T>::dispose_all(std::string name) {
-		if (this->available(name)) {
-			auto m = this->table[name];
-			m->clear();
+	return hash;
+}
+
+template <typename T>
+void thunderclap<T>::dispose(std::string name, std::string hash)
+{
+	if (this->available(name)) {
+		auto m = this->table[name];
+		auto it = m->find(hash);
+		if (it != m->end()) {
+			m->erase(it);
 		}
 	}
+}
+
+template <typename T>
+void thunderclap<T>::dispose_all(std::string name)
+{
+	if (this->available(name)) {
+		auto m = this->table[name];
+		m->clear();
+	}
+}
 }
--- include/thunderclap.h
+++ include/thunderclap.h
@@ -8,25 +8,28 @@
 
 #pragma once
 
-#include <vector>
-#include <string>
+#include <functional>
 #include <map>
 #include <memory>
-#include <functional>
+#include <string>
+#include <vector>
 
-namespace thunderclap {
-	template <typename T> class thunderclap
-	{
-	protected:
-		std::map<std::string, std::shared_ptr<std::map<std::string, std::function<void(T x)>>>> table;
-		bool available(std::string name);
-	public:
-		static thunderclap<T>* shared_instance();
-		void clap(std::string name, T x);
-		std::string watch(std::string name, std::function<void(T x)> f);
-		void dispose(std::string name, std::string hash);
-		void dispose_all(std::string name);
-	};
+namespace m2d
+{
+template <typename T>
+class thunderclap
+{
+protected:
+	std::map<std::string, std::shared_ptr<std::map<std::string, std::function<void(T x)>>>> table;
+	bool available(std::string name);
+
+public:
+	static thunderclap<T> *shared_instance();
+	void clap(std::string name, T x);
+	std::string watch(std::string name, std::function<void(T x)> f);
+	void dispose(std::string name, std::string hash);
+	void dispose_all(std::string name);
+};
 }
 
 #include "detail/thunderclap.h"
